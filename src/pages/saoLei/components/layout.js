b7a94a740ea23eb19758076fe6e3374ef51a0d35
gaoyunlong <gaoyunlong>
2021-09-26 16:59:31 +0800

    更新 saolei

--- src/pages/saoLei/components/layout.js
+++ src/pages/saoLei/components/layout.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import {message} from 'antd';
 import '../style/style.css'
+import leidian from '../image/leidian.png'
+import hua from '../image/hua.png'
+import flag from '../image/flag.png'
 
 export default class boxMain extends Component {
     constructor(props) {
@@ -13,9 +17,12 @@ export default class boxMain extends Component {
             result: 'playing',
         };
     }
+    isClick = false;
+    clickNum = 0;
 
     componentDidMount() {
         this.makeData();
+        this.props.onRef(this)
     }
 
     clone( obj ){
@@ -38,10 +45,20 @@ export default class boxMain extends Component {
         }
     }
 
+    changeStage( stage ){
+        let hig = 9, tol = 10;
+        if( stage === 'hard' ){
+            hig = 15;
+            tol = 20
+        }
+        this.setState( {hig: hig, tol: tol}, () => {
+            this.makeData();
+        } )
+    }
+
     makeData(){
-        let { data, tol, hig, wid } = this.state;
-        let nums = [];
-        let img = [];
+        let { tol, hig, wid } = this.state;
+        let nums = [], data = [], img = [];
         for( let i=0; i<( wid * hig ); i++ ){
             data.push( 0 );
         }
@@ -62,48 +79,106 @@ export default class boxMain extends Component {
             nums[page].push( item );
             img[page].push(0);
         } );
-        this.setState( { data: nums, img: img } )
+        console.log( 'startStatus', nums )
+        this.setState( { data: nums, img: img, result: 'playing' } )
     }
 
     onClick( e, index, childIndex, childItem, img, data ){
-        if( e === 0 ){                      // 左键点击
-            this.onCellClick( index, childIndex, childItem, img )
-        } else if( e === 1 ){               // 中键
-
-        } else if( e === 2 ){               // 右键
-
+        // if( e === 0 ){                      // 左键点击
+        //     this.onCellClick( index, childIndex, childItem, img )
+        // } else if( e === 1 ){               // 中键
+        //
+        // } else if( e === 2 ){               // 右键
+        //
+        // }
+        this.clickNum++;
+        if (this.isClick) {
+            return;
+        }
+        this.isClick = true;
+        setTimeout(() => {
+            // 超过1次都属于双击
+            if (this.clickNum > 1) {
+                this.doubleClick( index, childIndex, img );
+            } else {
+                this.onCellClick( index, childIndex, childItem, img );
+            }
+            this.clickNum = 0;
+            this.isClick = false;
+        }, 300);
+    }
+    showInfo(){
+        const { result } = this.state;
+        if( result === 'lose' ) {
+            message.warning('You Are Lose');
+        } else if ( result === 'win' ) {
+            message.success('You Are Win!');
         }
     }
-
+    doubleClick( index, childIndex, img ){
+        let imgNew = this.clone( img );
+        if( imgNew[index][childIndex] === 3 ){
+            imgNew[index][childIndex] = 0
+        } else if( imgNew[index][childIndex] === 0 ){
+            imgNew[index][childIndex] = 3;
+        }
+        this.setState( { img: imgNew } )
+    }
     onCellClick( index, childIndex, childItem, img ){
         let imgNew = this.clone( img );
-        const { data } = this.state;
-        if( childItem === 1 ){           // 当点击到炸弹
-            this.setState( { result: 'lose' } );
+        const { result } = this.state;
+        if( result !== 'playing' ){
+            this.showInfo();
+            return;
+        }
+        if( imgNew[index][childIndex] === 3 ) return;
+        if( childItem === 1 ){     // 当点击到炸弹
+            this.setState( { result: 'lose' }, () => {
+                this.showInfo();
+            } );
         } else if( childItem === 0 ){   //当没有点击到炸弹， 判断周围是否可以打开
-            console.log( 222, this.totalNum(index, childIndex) )
             if( this.totalNum(index, childIndex) !== 0 ){      // 周围有雷，不打开
                 imgNew[index][childIndex] = 1;
-                this.setState( { img: imgNew } )
+                this.setState( { img: imgNew }, () => {
+                    this.checkWin();
+                } )
             } else if( this.totalNum( index, childIndex) === 0 ){   //周围没雷，可以打开
-                let imgN = this.makeBlack( imgNew, index, childIndex, data );
-                console.log( 111, imgN, imgNew )
-                this.setState({ img: imgN })
+                let imgN = this.makeBlack( imgNew, index, childIndex );
+                this.setState({ img: imgN }, () => {
+                    this.checkWin();
+                } )
             }
         }
     }
+    checkWin(){
+        const { img, wid, hig, tol } = this.state;
+        let total = 0;
+        for( let i = 0; i<wid; i++ ){
+            for( let j=0; j<hig; j++ ){
+                if( img[i][j] !== 0 && img[i][j] !== 3 ) total++;
+            }
+        }
+        if( total === wid*hig - tol ){
+            this.setState( { result: 'win' }, () => {
+                this.showInfo();
+            } );
+        }
+    }
 
-    makeBlack( img, index, childIndex, data  ){
+    makeBlack( img, index, childIndex  ){
+        const { data } = this.state;
         let imgNew = this.clone( img );
         for( let i=-1; i<2; i++ ){
             for( let j=-1; j<2; j++ ){
-                if( data[ index+i ] && data[ index+i ][ childIndex+j ] && img[index+i][childIndex+j] !== 0){
-                } else if( i === 0 && j === 0 ){
-                    imgNew[ index+i ][ index+j ] = 2;
-                } else if( data[ index+i ] && data[ index+i ][ childIndex+j ] && this.totalNum( index+i, childIndex+j ) === 0 ){
-                    imgNew = this.makeBlack( imgNew,index+i, childIndex+j, data );
-                } else if( data[ index+i ] && data[ index+i ][ childIndex+j ] && this.totalNum( index+i, childIndex+j ) !== 0 ){
-                    imgNew[ index+i ][ index+j ] = 2;
+                if( data[ index+i ] && imgNew[ index+i ][ childIndex+j ] === 0 ){
+                    if( i === 0 && j === 0 && data[ index ][ childIndex ] === 0 ){
+                        imgNew[ index ][ childIndex ] = 2;
+                    } else if( data[ index+i ] && data[ index+i ][ childIndex+j ] === 0 && ( this.totalNum( index+i, childIndex+j ) === 0 ) ){
+                        imgNew[ index+i ][ childIndex+j ] = 2;
+                        imgNew = this.makeBlack( imgNew,index+i, childIndex+j );
+                    } else if( data[ index+i ] && data[ index+i ][ childIndex+j ] === 0 && ( this.totalNum( index+i, childIndex+j ) !== 0 ) ){
+                        imgNew[ index+i ][ childIndex+j ] = 1;
+                    }
                 }
             }
         }
@@ -127,7 +202,6 @@ export default class boxMain extends Component {
     render() {
         const { data, img, result } = this.state;
         if (!data) return false;
-        console.log( 99, data )
         return (
             <div style={ { display: 'table' } } >
                 {
@@ -142,13 +216,17 @@ export default class boxMain extends Component {
                                                     height: '30px',
                                                     border: '3px solid #FFF',
                                                     textAlign: 'center',
-                                                    color: '#fff',
-                                                    background: img[index][childIndex] === 1 || img[index][childIndex] === 2 ? '#fff' : 'rgba(1,1,1, 0.1)'
+                                                    color: '#000',
+                                                    background: result !== 'playing'?
+                                                        (result === 'lose' ? `url(${leidian}) no-repeat 0 0 /100% 100%` : `url(${hua}) no-repeat 0 0 /100% 100%`)
+                                                        : (img[index][childIndex] === 1 || img[index][childIndex] === 2 ?
+                                                        '#fff' : ( img[index][childIndex] === 0 ? 'rgba(1,1,1, 0.1)' : `url(${flag}) no-repeat 0 0 /100% 100%`)
+                                                        ),
                                                 } }
                                                 onMouseDown={ ( e ) => {
                                                     this.onClick( e.button, index, childIndex, childItem, img, data );
                                                 } }
-                                    > { result === 'lose' ? 'lose' :( img[index][childIndex] === 1 ? this.totalNum( index, childIndex ) : '' ) } </div>
+                                    > { result !== 'playing' ? '' :( img[index][childIndex] === 1 ? this.totalNum( index, childIndex ) : '' ) } </div>
                                 } )
                             }
                         </div>
--- src/pages/saoLei/main.js
+++ src/pages/saoLei/main.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Layout from './components/layout'
+import {Button, Select, message} from 'antd';
+import {InfoOutlined} from '@ant-design/icons'
 
 export default class SaoLei extends React.Component{
+    newGame = () => {
+        this.child.makeData();
+    }
+    onRef = (ref) => {
+        this.child = ref
+    }
+    changeStage = (e) => {
+        this.child.changeStage(e)
+    }
+    showTip(){
+        message.info('可以双击来标记 🚩');
+    }
     render(){
         return(
             <div>
-                <Layout/>
+                <div style={{ display: 'block', marginBottom: '20px' }}>
+                    <Button type="primary" shape="round" size='small' onClick={() => this.newGame()}>
+                        New Game
+                    </Button>
+                    <Button type="primary" style={{ marginLeft: '20px' }} icon={<InfoOutlined />} shape="circle" size='small' onClick={() => this.showTip()}/>
+                    <Select style={{ marginLeft: '20px' }} defaultValue="easy" size='small' onChange={(e) => this.changeStage(e)}>
+                        <Select.Option value="easy">Easy</Select.Option>
+                        <Select.Option value="hard">Hard</Select.Option>
+                    </Select>
+                </div>
+                <Layout onRef={this.onRef}/>
             </div>
         )
     }
