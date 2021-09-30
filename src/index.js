b711636acb303e6f2d0c871cd806260a697a1b19
gaoyunlong <gaoyunlong>
2021-09-30 16:00:13 +0800

    更新 router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Snick from './pages/snick/main';
+import GameMain from './pages/gameMain';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
@@ -9,7 +10,8 @@ ReactDOM.render(
   <React.StrictMode>
       <Router>
           <Route path="/" exact component={App}/>
-          <Route path="/snick" component={Snick}/>
+          <Route path="/games/snick" component={Snick}/>
+          <Route path="/games/main" component={GameMain}/>
       </Router>
   </React.StrictMode>,
   document.getElementById('root')
